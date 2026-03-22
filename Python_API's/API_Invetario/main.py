7610bd67354b89f64f8720e0dc19473777bf83f8
Vitaum <vitinho6033@gmail.com>
2026-03-22 03:31:53 -0300

    Primeira API em desenvimento

---
+++ Python_API's/API_Invetario/main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+#Criando uma classe de Usuario (oque sera solicitado ao cliente para preenchimento)
+class usuario(BaseModel):
+    nome: str
+    idade: int
+
+#criar uma lista para armazenar os usuarios
+usuarios = []
+contador_id = 1
+
+@app.get("/")
+def home():
+    return {"message": "API em funcionamento!"}
+
+#Criar um endpoint para criar um usuario
+@app.post("/usuarios")
+def criar_usuario(usuario: usuario):
+    global contador_id
+
+    #Oque sera armazenado para cada usuario criado, o id é gerado automaticamente
+    novo_usuario = {
+        "id": contador_id,
+        "nome": usuario.nome,
+        "idade": usuario.idade
+    }
+
+    usuarios.append(novo_usuario) #armazena o usuario na lista de usuarios
+    contador_id += 1 #Adiciona automaticamente um id para cada usuario criado
+
+    return {
+        "mensagem": "Usuario criado com sucesso!",
+            "usuario": novo_usuario
+            }
+
+#Lista os Usuarios na lista Usuarios
+@app.get("/usuarios")
+def listar_usuarios():
+    return usuarios
