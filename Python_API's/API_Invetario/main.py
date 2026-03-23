7881bb45f434822365c12f0f8ca484de655c8837
Vitaum <vitinho6033@gmail.com>
2026-03-22 23:18:35 -0300

    Finalizacao do CRUD de Usuarios, agora é possível criar, listar, obter, atualizar e deletar usuarios.

--- Python_API's/API_Invetario/main.py
+++ Python_API's/API_Invetario/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi import HTTPException
 from pydantic import BaseModel
 
 #Para iniciar a API, use no bash o comando abaixo:
@@ -6,12 +7,12 @@ from pydantic import BaseModel
 app = FastAPI()
 
 #Criando uma classe de Usuario (oque sera solicitado ao cliente para preenchimento)
-class usuario(BaseModel):
+class Usuario(BaseModel):
     nome: str
     idade: int
 
 #criar uma lista para armazenar os usuarios
-usuarios = []
+Usuarios = []
 contador_id = 1
 
 @app.get("/")
@@ -19,26 +20,54 @@ def home():
     return {"message": "API em funcionamento!"}
 
 #Criar um endpoint para criar um usuario
-@app.post("/usuarios")
-def criar_usuario(usuario: usuario):
+@app.post("/Usuarios")
+def criar_usuario(Dados_Usuario: Usuario):
     global contador_id
 
     #Oque sera armazenado para cada usuario criado, o id é gerado automaticamente
-    novo_usuario = {
+    Novo_Usuario = {
         "id": contador_id,
         "nome": usuario.nome,
         "idade": usuario.idade
     }
 
-    usuarios.append(novo_usuario) #armazena o usuario na lista de usuarios
+    Usuarios.append(Novo_Usuario) #armazena o usuario na lista de usuarios
     contador_id += 1 #Adiciona automaticamente um id para cada usuario criado
 
     return {
         "mensagem": "Usuario criado com sucesso!",
-            "usuario": novo_usuario
+            "usuario": Novo_Usuario
             }
 
 #Lista os Usuarios na lista Usuarios
 @app.get("/usuarios")
 def listar_usuarios():
-    return usuarios
+    return Usuarios
+
+
+@app.get("/usuarios/{usuario_id}")
+def obter_usuario(usuario_id: int):
+    for Usuario in Usuarios:
+        if Usuario["id"] == usuario_id:
+            return Usuario
+        
+    raise HTTPException(status_code=404, detail="Usuario não encontrado")
+
+@app.delete("/usuarios/{usuario_id}")
+def deletar_usuario(usuario_id: int):
+    for i in range(len(Usuarios)):
+        if Usuarios[i]["id"] == usuario_id:
+            Usuarios.pop(i)
+            return {"mensagem": "Usuario deletado com sucesso!"}
+        
+    raise HTTPException(status_code=404, detail="Usuario não encontrado")
+
+@app.put("/usuarios/{usuario_id}")
+def atualizar_usuario(usuario_id: int, usuario_atualizado: Usuario):
+    for i in range(len(Usuarios)):
+        if Usuarios[i]["id"] == usuario_id:
+            Usuarios[i]["nome"] = usuario_atualizado.nome
+            Usuarios[i]["idade"] = usuario_atualizado.idade
+            return {"mensagem": "Usuario atualizado com sucesso!", "usuario": Usuarios[i]}
+        
+    raise HTTPException(status_code=404, detail="Usuario não encontrado")
